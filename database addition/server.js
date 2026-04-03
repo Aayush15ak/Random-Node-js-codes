55a5b4d2e2e15124ad05f840b6704643cda3cc58
Rahul Sharma <quake.05.005@gmail.com>
2026-04-03 19:50:49 +0530

    post fn

--- database addition/server.js
+++ database addition/server.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('./db'); // Import the database connection
+const Person = require('./models/employee'); // Import the Person model
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/add-person', async (req, res) => {
+    try {
+        const data =  req.body;
+        const newPerson = new Person(data);
+        await newPerson.save();
+        res.status(201).json({ message: 'Person added successfully',
+            person: newPerson
+         });
+    }
+    catch(err){
+        res.status(400).json({ error: 'Error adding person',
+            details: err.message
+         });
+    }
+});
 
 app.get('/', (req, res) => {
   res.send('First server!');
