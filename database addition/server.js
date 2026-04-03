979b2c03ab9334547620d6f0d5ef039cdb317431
Rahul Sharma <quake.05.005@gmail.com>
2026-04-03 21:18:23 +0530

    get fn of db

--- database addition/server.js
+++ database addition/server.js
@@ -3,9 +3,20 @@ const app = express();
 const port = 3000;
 const db = require('./db'); // Import the database connection
 const Person = require('./models/employee'); // Import the Person model
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.get('/people', async (req, res) => {
+    try {
+        const people = await Person.find();
+        res.status(200).json(people);
+    } catch (err) {
+        res.status(500).json({ error: 'Error fetching people',
+            details: err.message
+         });
+    }
+});
+
 
 app.post('/add-person', async (req, res) => {
     try {
